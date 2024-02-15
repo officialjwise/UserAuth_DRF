611fd12d5d50bd4eb8e3bdd4ceb2f0d29578c448
officialjwise <officialjwise20@gmail.com>
2024-02-15 22:36:44 +0000

    Urls added

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,10 @@
-from django.urls import re_path
-from .import views 
+from django.urls import path
+from .views import login_view, signup_view, update_user, custom_logout, test_token
 
 urlpatterns = [
-    re_path('login', views.login),
-    re_path('signup', views.signup),
-    re_path('test_token', views.test_token),
-
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('update_user/', update_user, name='update_user'),
+    path('logout/', custom_logout, name='custom_logout'),
+    path('test_token/', test_token, name='test_token'),
 ]
